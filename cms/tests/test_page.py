6ab6ad67af04f55c5d9338c90ffcbd7ce66a75cf
Nolan Stover <nstover@fusionbox.com>
2016-07-14 11:57:32 -0600

    Fixed page admin's views' incorrect selection of available languages
    Now get languages from site-specific cms admin site rather than the
    current site if the obj doesn't exist in the view.
    
    Getting the language from the request now gets available languages from
    cms_admin_site in request.session if current_page is not present. If the
    language is not in that set, use the first available from the set.

--- cms/tests/test_page.py
+++ cms/tests/test_page.py
@@ -623,6 +623,7 @@ class PagesTestCase(CMSTestCase):
             page.publish('en')
             req.current_page = page
             req.GET = {}
+            req.session = {}
             self.assertEqual(t.render(template.Context({"request": req})), "Hello I am a page")
 
     def test_page_obj_change_data_from_template_tags(self):
@@ -652,6 +653,7 @@ class PagesTestCase(CMSTestCase):
             after_change = tz_now()
             req.current_page = page
             req.GET = {}
+            req.session = {}
 
             actual_result = t.render(template.Context({"request": req}))
             desired_result = "{0} changed on {1}".format(
